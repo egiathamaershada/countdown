7a7512d6d0e965d890d39269b29ad09affd474cd
egiathamaershada <thama.egia@gmail.com>
2018-12-31 21:54:14 +0700

    update init

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import "../css/style.css"
+
+const testBabel = (name) => {
+    console.log(name)
+}
+
+testBabel("ethers")
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,53 @@
-var path = require('path');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src') + '/js/index.js',
+  entry: path.resolve(__dirname, 'src/js') + '/index.js',
+  plugins: [
+    new CleanWebpackPlugin(['dist','index.html']),
+    new HtmlWebpackPlugin({
+      // filename: '../index.html',
+      template: 'template.html',
+      alwaysWriteToDisk: true
+    }),
+    new HtmlWebpackHarddiskPlugin({
+      outputPath: path.resolve(__dirname, '')
+    })
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    publicPath: 'dist',
+    filename: 'bundle.[hash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        include: [
+          path.resolve(__dirname, "src/js")
+        ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /(node_modules)/,
+        include: [
+          path.resolve(__dirname, "src/css")
+        ],
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader'
+        ]
+      }
+    ]
   }
 };
